939ef26bd8035f19cfae1016cc207f8e5538daaf
Sander <sander@recras.nl>
2019-11-28 16:24:35 +0100

    Code style

--- ace/plugin.js
+++ ace/plugin.js
@@ -6,98 +6,117 @@
  * Minor edits by Recras - devs@recras.nl
  */
 
-tinymce.PluginManager.add("ace", function(editor) {
-	function aceEditor() {
-		var innerWidth = $(window).innerWidth() - 30,
-			innerHeight = $(window).innerHeight() - 120;
-		innerWidth > 1800 && (innerWidth = 1800), innerHeight > 1200 && (innerHeight = 1200);
+tinymce.PluginManager.add('ace', function(editor) {
+    function aceEditor() {
+        let innerWidth = $(window).innerWidth() - 30;
+        let innerHeight = $(window).innerHeight() - 120;
 
-		var x = (innerWidth - 20) % 138;
+        innerWidth > 1800 && (innerWidth = 1800);
+        innerHeight > 1200 && (innerHeight = 1200);
 
-		if (innerWidth = innerWidth - x + 10, innerWidth > 600) {
-			var x = (innerWidth - 20) % 138;
-			innerWidth = innerWidth - x + 10
-		}
+        let x = (innerWidth - 20) % 138;
 
-		editor.windowManager.open({
-			title: "Source code",
-			type: "container",
-			id: "mce-ace-editor",
-			name: "ace",
-			width: innerWidth,
-			height: innerHeight,
-			inline: 1,
-			buttons:[
-				{
-					text: "Ok",
-					classes: "primary",
-					onclick : function() {
-						if(typeof mce_editor !== "undefined") {
-							var html = mce_editor.getValue();
+        innerWidth = innerWidth - x + 10;
+        if (innerWidth > 600) {
+            x = (innerWidth - 20) % 138;
+            innerWidth = innerWidth - x + 10;
+        }
 
-							editor.focus(), editor.undoManager.transact(function() {
-								editor.setContent(html)
-							}), editor.selection.setCursorLocation(), editor.nodeChanged();
+        editor.windowManager.open({
+            title: 'Source code',
+            type: 'container',
+            id: 'mce-ace-editor',
+            name: 'ace',
+            width: innerWidth,
+            height: innerHeight,
+            inline: 1,
+            buttons: [
+                {
+                    text: 'Ok',
+                    classes: 'primary',
+                    onclick: function() {
+                        if (typeof mce_editor !== 'undefined') {
+                            const html = mce_editor.getValue();
 
-							editor.windowManager.close();
-						}
-					}
-				},
-				{
-					text: "Cancel",
-					onclick: "close"
-				}
-			]
-		});
+                            editor.focus();
+                            editor.undoManager.transact(function() {
+                                editor.setContent(html);
+                            });
+                            editor.selection.setCursorLocation();
+                            editor.nodeChanged();
 
-		if($('#mce-ace-editor-body').length) {
-			$('#mce-ace-editor-body').append('<div id="mce-ace-editor-block"></div>');
+                            editor.windowManager.close();
+                        }
+                    },
+                },
+                {
+                    text: 'Cancel',
+                    onclick: 'close',
+                },
+            ],
+        });
 
-			// Load editor
-			$('#mce-ace-editor-block').css({'position':'absolute', 'left':'0', 'right':'0', 'top':'0', 'bottom':'0'});
+        if ($('#mce-ace-editor-body').length) {
+            $('#mce-ace-editor-body').append('<div id="mce-ace-editor-block"></div>');
 
-			var mce_editor = ace.edit('mce-ace-editor-block');
-			mce_editor.getSession().setMode("ace/mode/html");
-			mce_editor.setOptions({
-				showPrintMargin: false
-			});
-			mce_editor.getSession().setUseWrapMode(true);
+            // Load editor
+            $('#mce-ace-editor-block').css({
+                bottom: 0,
+                left: 0,
+                position: 'absolute',
+                right: 0,
+                top: 0,
+            });
 
-			// Set editor contents
-			mce_editor.getSession().setValue(editor.getContent({
-				source_view: !0
-			}));
-		}
+            let mce_editor = ace.edit('mce-ace-editor-block');
+            mce_editor.getSession().setMode('ace/mode/html');
+            mce_editor.setOptions({
+                showPrintMargin: false,
+            });
+            mce_editor.getSession().setUseWrapMode(true);
 
-		// Only thing needed to get Ace to work is to remove the mce-container class
-		// Tinymce re-adds the class so we need to loop it
-		// TODO: Find a less HORRIFIC solution for this
-		var countchecks = 0;
+            // Set editor contents
+            mce_editor.getSession().setValue(
+                editor.getContent({
+                    source_view: true,
+                })
+            );
+        }
 
-		function soddoffmce() {
-			// Lets do it say 50 times (for 5 seconds)...
-			if(countchecks++ == 50) {
-				clearTimeout(soddoffloop);
-				return;
-			}
+        // Only thing needed to get Ace to work is to remove the mce-container class
+        // Tinymce re-adds the class so we need to loop it
+        // TODO: Find a less HORRIFIC solution for this
+        let countchecks = 0;
+        let soddoffloop;
 
-			soddoffloop = setTimeout(function () {
-				if($('#mce-ace-editor').hasClass('mce-container')) $('#mce-ace-editor').removeClass('mce-container');
-				soddoffmce();
-			}, 100);
-		}
+        function soddoffmce() {
+            // Lets do it say 50 times (for 5 seconds)...
+            if (countchecks++ === 50) {
+                clearTimeout(soddoffloop);
+                return;
+            }
 
-		soddoffmce();
-	}
+            soddoffloop = setTimeout(function() {
+                if ($('#mce-ace-editor').hasClass('mce-container')) {
+                    $('#mce-ace-editor').removeClass('mce-container');
+                }
+                soddoffmce();
+            }, 100);
+        }
 
-	editor.addCommand("mceace_codeEditor", aceEditor), editor.addButton("ace", {
-		icon: "code",
-		tooltip: "Source code",
-		onclick: aceEditor
-	}), editor.addMenuItem("ace", {
-		icon: "code",
-		text: "Source code",
-		context: "tools",
-		onclick: aceEditor
-	})
+        soddoffmce();
+    }
+
+    editor.addCommand('mceace_codeEditor', aceEditor);
+    editor.addButton('ace', {
+        icon: 'code',
+        tooltip: 'Source code',
+        onclick: aceEditor,
+    });
+    editor.addMenuItem('ace', {
+        icon: 'code',
+        text: 'Source code',
+        context: 'tools',
+        onclick: aceEditor,
+    });
 });
